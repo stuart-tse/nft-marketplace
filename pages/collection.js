cdade6e1ac4630aee4e608d6c1fb9557859e211b
Stuart Tse <stuart_tse@outlook.com>
2022-12-11 03:58:20 +0800

    Added Collection Page

---
+++ pages/collection.js
@@ -0,0 +1,39 @@
+
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/collection.module.css";
+import images from "../assets/img";
+import {
+    Banner,
+    CollectionProfile,
+    NFTCardTwo,
+} from "../collectionPage/collectionIndex";
+import { Slider, Brand } from "../components/componentsindex";
+import Filter from "../components/Filter/Filter";
+
+const collection = () => {
+    const collectionArray = [
+        images.nftImage1,
+        images.nftImage2,
+        images.nftImage3,
+        images.nftImage1,
+        images.nftImage2,
+        images.nftImage3,
+        images.nftImage1,
+        images.nftImage2,
+    ];
+    return (
+        <div className={Style.collection}>
+            <Banner bannerImage={images.creatorbackground1} />
+            <CollectionProfile />
+            <Filter />
+            <NFTCardTwo NFTData={collectionArray} />
+
+            <Slider />
+            <Brand />
+        </div>
+    );
+};
+
+export default collection;
